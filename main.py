377943eef4756bac2736b93db86d25c3b20da21a
konotopnazar <konotopnazar05@gmail.com>
2024-05-05 21:58:01 +0300

    Draw the triangle

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+if __name__ == '__main__':
+    a = 200
+    def draw():
+        fd(a)
+        left(120)
+        fd(a)
+        left(120)
+        fd(a)
+        left(120)
+
+
+    while a >= 20:
+        draw()
+        a -= 20
---
+++ Конотоп Назар завдання 7.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+if __name__ == '__main__':
+    a = 200
+    def draw():
+        fd(a)
+        left(120)
+        fd(a)
+        left(120)
+        fd(a)
+        left(120)
+
+
+        Turtle()
+    while a >= 20:
+        draw()
+        a -= 20
